327d42a9d3858aae0df02107402964ec2afcde72
Chester Chu <chester.yw.chu@gmail.comchester.yw.chu@gmail.com>
2017-06-08 21:43:56 +0800

    add facecrop.py

---
+++ facecrop.py
@@ -0,0 +1,32 @@
+import cv2
+
+def facechop(image):  
+    facedata = "haarcascade_frontalface_default.xml"
+    cascade = cv2.CascadeClassifier(facedata)
+
+    img = cv2.imread(image)
+
+    minisize = (img.shape[1],img.shape[0])
+    miniframe = cv2.resize(img, minisize)
+
+    faces = cascade.detectMultiScale(miniframe)
+
+    for f in faces:
+        x, y, w, h = [ v for v in f ]
+        cv2.rectangle(img, (x,y), (x+w,y+h), (255,255,255))
+
+        sub_face = img[y:y+h, x:x+w]
+        face_file_name = "faces/face_" + str(y) + ".jpg"
+        cv2.imwrite(face_file_name, sub_face)
+
+    cv2.imshow(image, img)
+
+    return
+
+if __name__ == '__main__':  
+    facechop("IMAG2618.jpg")
+
+    while(True):
+        key = cv2.waitKey(20)
+        if key in [27, ord('Q'), ord('q')]: 
+            break
--- predict.py
+++ predict.py
@@ -6,40 +6,47 @@ import sys
 import PIL
 
 # Usage: python predict.py classifiers/classifier_batch200_augmented_val_acc_0.5305.h5 data/fer2013_private_test.csv tmp
+# (0=Angry, 1=Disgust, 2=Fear, 3=Happy, 4=Sad, 5=Surprise, 6=Neutral)
 
 batch_size = 1
 img_width = 48
 img_height = 48
+
+emotions = ['Angry', 'Disgust', 'Fear', 'Happy', 'Sad', 'Surprise', 'Neutral']
+
+def get_emotion(predictions):
+	emotion = emotions[np.argmax(predictions)]
+	return emotion
 
 if __name__ == "__main__":
 	print('Load model:' + sys.argv[1])
 	model = load_model(sys.argv[1])
 	model.summary()  #Verifying model structure
 
-	df = pd.read_csv(sys.argv[2])
-	X = df['pixels'].as_matrix()
-	Y = df['emotion'].as_matrix()
+	# df = pd.read_csv(sys.argv[2])
+	# X = df['pixels'].as_matrix()
+	# Y = df['emotion'].as_matrix()
 
-	tmp = []
-	for x in X:
-		tmp.append([int(p) for p in x.split(' ')])
-	X = np.array(tmp)
-	X = X.reshape(X.shape[0], img_width, img_height, 1)
-	print(X.shape)
+	# tmp = []
+	# for x in X:
+	# 	tmp.append([int(p) for p in x.split(' ')])
+	# X = np.array(tmp)
+	# X = X.reshape(X.shape[0], img_width, img_height, 1)
+	# print(X.shape)
 
-	i = 0
-	datagen = ImageDataGenerator()
-	for batch in datagen.flow(X, batch_size=1, save_to_dir=sys.argv[3]+'/tmp', save_prefix='img', save_format='jpeg'):
-		i += 1
-		if i >= (X.shape[0]):
-			break
+	# i = 0
+	# datagen = ImageDataGenerator()
+	# for batch in datagen.flow(X, batch_size=1, save_to_dir=sys.argv[3]+'/tmp', save_prefix='img', save_format='jpeg'):
+	# 	i += 1
+	# 	if i >= (X.shape[0]):
+	# 		break
 
 
 
 
 	test_datagen = ImageDataGenerator(rescale=1. / 255)
 	test_generator = test_datagen.flow_from_directory(
-	    sys.argv[3],
+	    sys.argv[2],
 	    target_size=(img_width, img_height),
 	    batch_size=batch_size,
 	    color_mode='grayscale',
@@ -47,7 +54,11 @@ if __name__ == "__main__":
 
 
 
-	predictions = model.predict_generator(test_generator, X.shape[0])
+	# predictions = model.predict_generator(test_generator, X.shape[0])
+	predictions = model.predict_generator(test_generator, 1)
+	emotion = get_emotion(predictions)
 	print(predictions)
-	print(Y)
+	print(emotion)
+
+	# print(Y)
 
